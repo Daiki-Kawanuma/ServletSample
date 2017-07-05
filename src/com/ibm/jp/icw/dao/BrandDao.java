4558692cfc66ea49108129ff0be75f21c1fa09d7
aa367474 <aa367474@localhost>
2017-07-05 13:27:09 +0900

    BrandDaoのメソッド追加修正

--- src/com/ibm/jp/icw/dao/BrandDao.java
+++ src/com/ibm/jp/icw/dao/BrandDao.java
@@ -18,7 +18,7 @@ public class BrandDao extends BaseDao{
 	public static final String COLUMN_TRADING_UNIT = "TRADING_UNIT";
 	public static final String COLUMN_BRAND_STATUS = "BRAND_STATUS";
 
-	public static Brand getBrand(String brandCode){
+	public static Brand getBrandByBrandCode(String brandCode){
 
 		Brand brand = null;
 		Connection connection = null;
@@ -32,7 +32,57 @@ public class BrandDao extends BaseDao{
 
 			// TODO クエリを書く
 			ResultSet resultSet = statement.executeQuery(
-					String.format("SELECT * FROM user WHERE accout_number = '%s'", brandCode));
+					String.format("SELECT * FROM brand WHERE brand_code = '%s'", brandCode));
+
+			if (resultSet.next()){
+				// TODO 数字を ResultSet から入れる
+				brand = new Brand(resultSet.getString(COLUMN_BRAND_CODE),
+						resultSet.getString(COLUMN_BRAND_NAME),
+						resultSet.getString(COLUMN_MARKET),
+						resultSet.getString(COLUMN_INDUSTRY),
+						resultSet.getInt(COLUMN_TRADING_UNIT),
+						resultSet.getString(COLUMN_BRAND_STATUS),
+						0,0,0,0,0,0,0,0);
+			}
+		} catch (SQLException e) {
+			System.err.println("エラー：BrandDao#DBデータ操作時にエラー発生");
+			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.err.println("エラー：BrandDao#Statementのクローズ処理時にエラー発生");
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.err.println("エラー：BrandDao#connectionのクローズ処理時にエラー発生");
+					e.printStackTrace();
+				}
+			}
+		}
+		return brand;
+	}
+
+	public static Brand getBrandByBrandName(String brandName){
+
+		Brand brand = null;
+		Connection connection = null;
+		Statement statement = null;
+
+		try {
+			connection = DriverManager.getConnection(DatabaseConstants.URL,
+					DatabaseConstants.USER,
+					DatabaseConstants.PASSWORD);
+			statement = connection.createStatement();
+
+			// TODO クエリを書く
+			ResultSet resultSet = statement.executeQuery(
+					String.format("SELECT * FROM brand WHERE brand_name = '%s'", brandName));
 
 			if (resultSet.next()){
 				// TODO 数字を ResultSet から入れる
