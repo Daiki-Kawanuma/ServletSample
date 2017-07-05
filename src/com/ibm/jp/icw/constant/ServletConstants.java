d8e225ece0a4f3dd7ee11adf25a391573f4af4a0
aa367318 <aa367318@localhost>
2017-07-05 16:23:06 +0900

    servletconstantsを更新しました

--- src/com/ibm/jp/icw/constant/ServletConstants.java
+++ src/com/ibm/jp/icw/constant/ServletConstants.java
@@ -4,7 +4,18 @@ public class ServletConstants {
 
 	public static final String MY_PAGE = "mypage";
 
+	public static final String ORDERED_INFO = "orderdinfo";
+
+	public static final String LOGIN = "login";
+	public static final String LOGOUT = "logout";
+	public static final String FORCED_LOGOUT = "forcedlogout";
+
 	public static final String ORDER_ENTRY = "orderentry";
 	public static final String ORDER_CONFIRM = "orderconfirm";
-	public static final String ORDER_COMPLETE = "order_complete";
+	public static final String ORDER_COMPLETE = "ordercomplete";
+
+	public static final String BRAND_SEARCH = "brandsearch";
+	public static final String BRAND_LIST = "brandlist";
+	public static final String BRAND_DETAIL = "branddetail";
+
 }
--- src/com/ibm/jp/icw/servlet/BrandInfoServlet.java
+++ src/com/ibm/jp/icw/servlet/BrandInfoServlet.java
@@ -1,13 +1,16 @@
 package com.ibm.jp.icw.servlet;
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.ibm.jp.icw.constant.ServletConstants;
 import com.ibm.jp.icw.constant.SessionConstants;
 import com.ibm.jp.icw.model.Brand;
+import com.ibm.jp.icw.model.Order;
 import com.ibm.jp.icw.model.User;
 
 //package com.ibm.jp.icw.servlet;
@@ -26,14 +29,51 @@ public class BrandInfoServlet extends BaseServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 
 		HttpSession session = request.getSession();
+		
 		User user = (User) session.getAttribute(SessionConstants.PARAM_USER);
+		Brand brand = (Brand) session.getAttribute(SessionConstants.PARAM_BRAND);
 
-		Brand.getBrand("jh");
+//		String currentPage = (String) request.getAttribute(PARAM_CURRENT_PAGE);
+//		String nextPage = null;
 
+		switch (currentPage) {
+		case ServletConstants.ORDER_ENTRY:
+
+			String orderType = (String) request.getAttribute(PARAM_ORDER_TYPE);
+			String orderCondition = (String) request.getAttribute(PARAM_ORDER_CONDITION);
+			String orderAmout  = (String) request.getAttribute(PARAM_ORDER_AMOUNT);
+			String orderUnitPrice = (String) request.getAttribute(PARAM_ORDER_UNIT_PRICE);
+
+			if(validateInputs(orderType, orderCondition, orderAmout,orderUnitPrice)){
+
+				nextPage = ServletConstants.ORDER_CONFIRM + ".jsp";
+
+			} else {
+
+				nextPage = ServletConstants.ORDER_ENTRY + ".jsp";
+
+				order = new Order(brand, user, Order.OrderType.getEnum(orderType),
+						Order.OrderCondition.getEnum(orderCondition),
+						Integer.parseInt(orderAmout), Integer.parseInt(orderUnitPrice), new Date());
+
+				session.setAttribute(SessionConstants.PARAM_ORDER, order);
+			}
+			break;
+		case ServletConstants.ORDER_CONFIRM:
+			nextPage = ServletConstants.ORDER_COMPLETE + ".jsp";
+			break;
+		case ServletConstants.ORDER_COMPLETE:
+			nextPage = ServletConstants.MY_PAGE;
+			break;
+		default:
+			break;
+		}
+
+		response.sendRedirect(nextPage);
 
 
 
