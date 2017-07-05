bb700dd38bdccdb98616a415f6f9a9a51b256322
aa367474 <aa367474@localhost>
2017-07-05 12:37:11 +0900

    MarketPriceDao作成

--- src/com/ibm/jp/icw/dao/MarketPriceDao.java
+++ src/com/ibm/jp/icw/dao/MarketPriceDao.java
@@ -1,10 +1,62 @@
 package com.ibm.jp.icw.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.ibm.jp.icw.constant.DatabaseConstants;
 import com.ibm.jp.icw.model.MarketPrice;
 
 public class MarketPriceDao extends BaseDao {
 
-	public static MarketPrice getFairPrice(){
-		return new MarketPrice();
+	public static final String COLUMN_BRAND_CODE = "BRAND_CODE";
+	public static final String COLUMN_DATE = "DATE";
+	public static final String COLUMN_PRICE = "PRICE";
+
+	public static MarketPrice getMarketPrice(String brandCode){
+
+		MarketPrice price = null;
+		Connection connection = null;
+		Statement statement = null;
+
+		try {
+			connection = DriverManager.getConnection(DatabaseConstants.URL,
+					DatabaseConstants.USER,
+					DatabaseConstants.PASSWORD);
+			statement = connection.createStatement();
+
+			// TODO クエリを書く
+			ResultSet resultSet = statement.executeQuery(
+					String.format("SELECT * FROM user WHERE accout_number = '%s'", brandCode));
+
+			if (resultSet.next()){
+				price = new MarketPrice(resultSet.getString(COLUMN_BRAND_CODE),
+						resultSet.getDate(COLUMN_DATE),
+						resultSet.getInt(COLUMN_PRICE));
+			}
+		} catch (SQLException e) {
+			System.err.println("エラー：BrandDao#DBデータ操作時にエラー発生");
+			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.err.println("エラー：BrandDao#Statementのクローズ処理時にエラー発生");
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.err.println("エラー：BrandDao#connectionのクローズ処理時にエラー発生");
+					e.printStackTrace();
+				}
+			}
+		}
+		return price;
 	}
 }
--- src/com/ibm/jp/icw/model/MarketPrice.java
+++ src/com/ibm/jp/icw/model/MarketPrice.java
@@ -4,18 +4,18 @@ import java.util.Date;
 
 public class MarketPrice {
 
-	private int brandCode;
+	private String brandCode;
 	private Date date;
 	private double price;
 
-	public MarketPrice(int brandCode, Date date, double price) {
+	public MarketPrice(String brandCode, Date date, double price) {
 		super();
 		this.brandCode = brandCode;
 		this.date = date;
 		this.price = price;
 	}
 
-	public int getBrandCode() {
+	public String getBrandCode() {
 		return brandCode;
 	}
 
