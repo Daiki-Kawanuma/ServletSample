87a6105bca36127ede5694b580125f9779c2d408
aa367474 <aa367474@localhost>
2017-07-05 13:15:55 +0900

    SessionConstants追加

---
+++ src/com/ibm/jp/icw/constant/SessionConstants.java
@@ -0,0 +1,9 @@
+package com.ibm.jp.icw.constant;
+
+public class SessionConstants {
+
+	public static final String PARAM_USER  = "USER";
+	public static final String PARAM_ACCOUNT_NUMBER = "ACCOUNT_NUMBER";
+	public static final String PARAM_BRAND = "BRAND";
+	public static final String PARAM_BRAND_CODE = "BRAND_CODE";
+}
--- src/com/ibm/jp/icw/servlet/OrderServlet.java
+++ src/com/ibm/jp/icw/servlet/OrderServlet.java
@@ -1,6 +1,7 @@
 package com.ibm.jp.icw.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,22 +12,20 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class OrderServlet
  */
 @WebServlet("/OrderServlet")
-public class OrderServlet extends HttpServlet {
+public class OrderServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public OrderServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
