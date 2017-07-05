0f8038a6154c8464bf598a1cc8d86073c6946beb
aa367474 <aa367474@localhost>
2017-07-05 12:54:34 +0900

    ログアウト機能作成

--- src/com/ibm/jp/icw/servlet/BaseServlet.java
+++ src/com/ibm/jp/icw/servlet/BaseServlet.java
@@ -1,11 +1,19 @@
 package com.ibm.jp.icw.servlet;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public abstract class BaseServlet extends HttpServlet {
 
+	public void doLogout(HttpServletRequest request, HttpServletResponse response){
 
-	public void doLogout(){
-
+		try{
+			request.getSession().invalidate();
+			response.sendRedirect("login.jsp");
+		} catch(Exception e){
+			System.err.println("エラー：BaseServlet#ログアウト失敗");
+			e.printStackTrace();
+		}
 	}
 }
