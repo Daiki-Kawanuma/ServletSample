80159aa0b0e94e1c0dcdf0e6d4f933afc078d3d8
aa367474 <aa367474@localhost>
2017-07-06 10:01:41 +0900

    [uodate]DBConstの更新

--- src/com/ibm/jp/icw/constant/DatabaseConstants.java
+++ src/com/ibm/jp/icw/constant/DatabaseConstants.java
@@ -4,5 +4,5 @@ public class DatabaseConstants {
 
 	public static final String URL = "jdbc:db2://dashdb-entry-yp-dal09-09.services.dal.bluemix.net:50000/BLUDB";
 	public static final String USER = "dash6930";
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "3tTdhUB8$gY#";
 }
