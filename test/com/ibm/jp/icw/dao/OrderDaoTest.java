d69233fa760595e5cb4d72f0c31a081773d290f9
aa370130 <aa370130@localhost>
2017-07-07 14:14:47 +0900

    渡井のテストはOrderDaoTestとOrderServletTestです

---
+++ test/com/ibm/jp/icw/dao/OrderDaoTest.java
@@ -0,0 +1,69 @@
+package com.ibm.jp.icw.dao;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ibm.jp.icw.dao.util.UtilDAO;
+import com.ibm.jp.icw.model.Order;
+
+public class OrderDaoTest {
+
+	UtilDAO utilDao = new UtilDAO();
+	String accountNumber;
+
+	@Before
+	public void setUp() throws Exception {
+		utilDao.init();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		utilDao.close();
+	}
+
+	@Test
+	public void 口座番号1000000000000000を入力すると過去の注文全てが取得される() {
+		accountNumber = "1000000000000000";
+		ArrayList<Order> list = OrderDao.getOrderList(accountNumber);
+		assertThat(list.size(), not(0));
+	}
+
+	@Test
+	public void 口座番号0000000000000000を入力するとnullが返される() {
+		accountNumber = "0000000000000000";
+		ArrayList<Order> list = OrderDao.getOrderList(accountNumber);
+		assertThat(list.size(), is(0));
+	}
+
+	@Test
+	public void 口座番号9999999999999を入力するとnullが返される() {
+		accountNumber = "99999999999999999";
+		ArrayList<Order> list = OrderDao.getOrderList(accountNumber);
+		assertThat(list.size(), is(0));
+	}
+
+	@Test
+	public void 口座番号を空欄にするとnullが返される() {
+		accountNumber = "";
+		ArrayList<Order> list = OrderDao.getOrderList(accountNumber);
+		assertThat(list.size(), is(0));
+	}
+
+	//川沼がやれ
+	@Test
+	public void () {
+
+		accountNumber = "1000000000000000";
+		brandCode = "1000";
+		Order order = OrderDao.registOrder(order);
+		assertThat(order.size(), is(0));
+	}
+}
+
+
--- test/com/ibm/jp/icw/servlet/OrderServletTest.java
+++ test/com/ibm/jp/icw/servlet/OrderServletTest.java
@@ -41,4 +41,160 @@ public class OrderServletTest {
 		// 検証
 		assertThat(result, is(true));
 	}
+
+	@Test
+	public void 成行ー引けー100ー100でTRUEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "引け";
+		orderAmount = "100";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void 指値ー無条件ー100ー100でTRUEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "指値";
+		orderCondition = "無条件";
+		orderAmount = "100";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void 指値ー寄付ー100ー100でTRUEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "指値";
+		orderCondition = "寄付";
+		orderAmount = "100";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void 指値ー引けー100ー100でTRUEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "指値";
+		orderCondition = "引け";
+		orderAmount = "100";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(true));
+	}
+	@Test
+	public void 任意ー指成ー100ー100でTRUEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "任意";
+		orderCondition = "指成";
+		orderAmount = "100";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(true));
+	}
+
+	@Test
+	public void 成行ー無条件ー99999999ー100でFALSEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "無条件";
+		orderAmount = "99999999";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void 成行ー無条件ーあああー100でFALSEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "無条件";
+		orderAmount = "あああ";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void 成行ー無条件ーnullー100でFALSEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "無条件";
+		orderAmount = "null";
+		orderUnitPrice = "100";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void 成行ー無条件ー100ー99999999でFALSEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "無条件";
+		orderAmount = "100";
+		orderUnitPrice = "99999999";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void 成行ー無条件ー100ーAAAでFALSEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "無条件";
+		orderAmount = "100";
+		orderUnitPrice = "AAA";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void 成行ー無条件ー100ーnullでFALSEを返す() {
+
+		// ユースケース２のパターン
+		orderType = "成行";
+		orderCondition = "無条件";
+		orderAmount = "100";
+		orderUnitPrice = "null";
+		// 実行
+		result = servlet.validateInputs(orderType, orderCondition, orderAmount, orderUnitPrice);
+		// 検証
+		assertThat(result, is(false));
+	}
+
+	//川沼クレジットカードのテストケースを書け
+
 }
