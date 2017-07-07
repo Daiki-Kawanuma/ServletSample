df3b29ad551eee7a7be3cd2a5ac6dff5bcd21059
aa367318 <aa367318@localhost>
2017-07-07 15:00:21 +0900

    BrandDaoTestを更新しました！

--- test/com/ibm/jp/icw/dao/BrandDaoTest.java
+++ test/com/ibm/jp/icw/dao/BrandDaoTest.java
@@ -1,14 +1,21 @@
 package com.ibm.jp.icw.dao;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import com.ibm.jp.icw.dao.util.UtilDAO;
+import com.ibm.jp.icw.model.Brand;
 
 public class BrandDaoTest {
 
 	UtilDAO utilDao = new UtilDAO();
-	String accountNumber;
+	String brandCode;
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,5 +25,33 @@ public class BrandDaoTest {
 	@After
 	public void tearDown() throws Exception {
 		utilDao.close();
+	}
+
+	@Test
+	public void getBrandbyBrandCode1000で正常に返る() {
+		brandCode = "1000";
+		ArrayList<Brand> list = BrandDao.getBrandByBrandCode(brandCode);
+		assertThat(list.size(), not(0));
+	}
+
+	@Test
+	public void getBrandbyBrandCode0000は正常に返らない() {
+		brandCode = "0000";
+		ArrayList<Brand> list = BrandDao.getBrandByBrandCode(brandCode);
+		assertThat(list.size(), is(0));
+	}
+
+	@Test
+	public void getBrandbyBrandCode99999は正常に返らない() {
+		brandCode = "99999";
+		ArrayList<Brand> list = BrandDao.getBrandByBrandCode(brandCode);
+		assertThat(list.size(), is(0));
+	}
+
+	@Test
+	public void etBrandbyBrandCod空は正常に返らない() {
+		brandCode = "";
+		ArrayList<Brand> list = BrandDao.getBrandByBrandCode(brandCode);
+		assertThat(list.size(), is(0));
 	}
 }
